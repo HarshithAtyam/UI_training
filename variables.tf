7dcd91de0e95fd23684d4ed2e1b08edc73e15f07
HarshithAtyam <harshith.atyam@arcadis.com>
2025-11-18 11:26:08 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,44 +1,27 @@
-# Service principal variables
 variable "tenant_id" {
-  description = "Azure Tenant ID."
   type        = string
   sensitive   = true
 }
 
 variable "client_id" {
-  description = "Service Principal Client ID."
   type        = string
   sensitive   = true
 }
 
 variable "client_secret" {
-  description = "Service Principal Client Secret."
   type        = string
   sensitive   = true
 }
 
-# Workspace & Capacity Variables
 variable "workspace_name" {
-  description = "Name of the workspace to create."
-  type        = string
+  type = string
 }
 
 variable "capacity_name" {
-  description = "Final mapped Fabric capacity name."
-  type        = string
+  type = string
 }
 
-variable "admin_email" {
-  description = "Email of admin to assign to the workspace."
-  type        = string
-}
-
-# AzureAD lookup for email → object_id
-data "azuread_user" "admin" {
-  user_principal_name = var.admin_email
-}
-
-# Convert lookup to a local variable
-locals {
-  admin_object_id = data.azuread_user.admin.object_id
+variable "admin_object_id" {
+  type = string
+  description = "Azure AD Object ID of user to be assigned as admin"
 }
